3f197c0f040050ddfbeed2a34c1e16b17aba8dea
Jeremy Nánási <Njeremy2002@gmail.com>
2025-09-12 10:59:52 +0200

    added movable-objects and changes in game.js

--- el_pollo_loco/js/game.js
+++ el_pollo_loco/js/game.js
@@ -1,15 +1,15 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MoveableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = './img/2_character_pepe/1_idle/idle/I-1.png';
+    // character.src = './img/2_character_pepe/1_idle/idle/I-1.png';
 
-    setTimeout( function() {
+    //     ctx.drawImage(character, 20, 20, 50, 150);
 
-        ctx.drawImage(character, 20, 20, 50, 150);
-    }, 2000)
+    console.log('My Character is', character);
+    
 }
 
