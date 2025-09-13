8b22c4970e864a8f6d36b101564841d6f14cf545
Jeremy Nánási <Njeremy2002@gmail.com>
2025-09-13 14:06:23 +0200

    el pollo loco - new files: background-objects.class.js, character.class.js, chicken.class.js, cloud.class.js, world.class.js, and changes in all this files + index, movealbe-objects.class.js and game.js

--- el_pollo_loco/js/game.js
+++ el_pollo_loco/js/game.js
@@ -1,15 +1,11 @@
 let canvas;
-let ctx;
-let character = new MoveableObject();
+let world;
 
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    // character.src = './img/2_character_pepe/1_idle/idle/I-1.png';
+    world = new World(canvas);
 
-    //     ctx.drawImage(character, 20, 20, 50, 150);
-
-    console.log('My Character is', character);
+    console.log('My Character is', world.character);
     
 }
 
---
+++ el_pollo_loco/js/models/world.class.js
@@ -0,0 +1,37 @@
+class World {
+    character = new Character();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    clouds = [
+        new Cloud()
+    ];
+    canvas;
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
+        this.draw();
+    }
+
+        draw() {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.heigt);
+            this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width);
+            this.enemies.forEach(enemy => {
+                this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width);
+            });
+
+            this.clouds.forEach(cloud => {
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.height, cloud.width);
+            });
+
+            // Draw() wird immer wieder aufgerufen
+            let self = this;
+            requestAnimationFrame(function() {
+                // self.draw();
+            });
+        }
+}
