3cf40bc4261844a7e1133713a9074f14e8ddf275
Jeremy Nánási <Njeremy2002@gmail.com>
2025-09-15 12:19:53 +0200

    moving of main character

--- el_pollo_loco/js/models/movable-object.class.js
+++ el_pollo_loco/js/models/movable-object.class.js
@@ -4,11 +4,25 @@ class MoveableObject {
     height = 150;
     width = 100;
     img;
+    imageChache = [];
 
     // loadImage('img/test.png');
     loadImage(path) {
         this.img = new Image();  // this.img = document.getElementById('image') <img id="image" src>
         this.img.src = path;
+    }
+
+    /**
+     * 
+     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ...] 
+     */
+    loadImages(arr) {
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageChache[path] = img;
+        })
+
     }
 
     moveRight() {
