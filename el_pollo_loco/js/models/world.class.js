3df99ce3838f1c51d7a749cf9e6b6c6314995b6d
Jeremy Nánási <Njeremy2002@gmail.com>
2025-09-23 10:58:25 +0200

    short time bug fixing. new issue: the picture is not loading

--- el_pollo_loco/js/models/world.class.js
+++ el_pollo_loco/js/models/world.class.js
@@ -19,7 +19,7 @@ class World {
     }
 
         draw() {
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.heigt);
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
             this.ctx.translate(this.camera_x, 0);
 
