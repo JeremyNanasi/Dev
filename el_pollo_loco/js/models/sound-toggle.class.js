6a78183c44843262f8c828d645446701e48c5f7f
Jeremy Nánási <Njeremy2002@gmail.com>
2026-01-27 21:00:49 +0100

    update Range of dmage and add damage.mp3

--- el_pollo_loco/js/models/sound-toggle.class.js
+++ el_pollo_loco/js/models/sound-toggle.class.js
@@ -1,207 +1,226 @@
-(function() {
-    if (window.EPL && window.EPL.Controllers && window.EPL.Controllers.SoundToggle) return;
-    window.EPL = window.EPL || {};
-    window.EPL.Controllers = window.EPL.Controllers || {};
+(function () {
+  window.EPL = window.EPL || {};
+  window.EPL.Controllers = window.EPL.Controllers || {};
 
-    function SoundToggleController(deps) {
-        this.deps = deps;
-        this.button = null;
-        this.icon = null;
-    }
-
-    SoundToggleController.prototype.init = function() {
-        this.button = document.getElementById('mute-toggle');
-        this.icon = document.getElementById('mute-icon');
-        if (!this.button || !this.icon) return;
-        this.attachListener();
-        this.updateIcon(this.deps.soundManager.isEnabled());
-    };
-
-    SoundToggleController.prototype.attachListener = function() {
-        let self = this;
-        this.button.addEventListener('click', function() {
-            let next = self.deps.soundManager.toggle();
-            self.updateIcon(next);
-        });
-    };
-
-    SoundToggleController.prototype.updateIcon = function(enabled) {
-        if (this.icon) {
-            this.icon.src = enabled ? './img/mobile/sound.png' : './img/mobile/mute.png';
-            this.icon.alt = enabled ? 'Sound an' : 'Sound aus';
-        }
-        if (this.button) {
-            this.button.setAttribute('aria-pressed', enabled ? 'false' : 'true');
-        }
-    };
-
-    window.EPL.Controllers.SoundToggle = SoundToggleController;
-
-    let SOUND_PATHS = {
-        smallchickenLoop: './img/effect-sound/small-chicken.mp3',
+  class EnemySfxManager {
+    constructor() {
+      this.stateMap = new WeakMap();
+      this.SOUND_PATHS = {
+        smallLoop: './img/effect-sound/small-chicken.mp3',
         chickenLoop: './img/effect-sound/chicken.mp3',
         death: './img/effect-sound/chicken-death.mp3',
         endbossAlert: './img/effect-sound/angry-chicken.mp4'
-    };
-
-    let RANGE = 200;
-    let INITIAL_DELAY_MIN = 400;
-    let INITIAL_DELAY_MAX = 1000;
-    let REPEAT_DELAY_MIN = 2600;
-    let REPEAT_DELAY_MAX = 3200;
-
-    class EnemySfxManager {
-        constructor() {
-            this.stateMap = new WeakMap();
-        }
-
-        update(world) {
-            if (!this.isEnabled()) {
-                this.stopAllActive(world);
-                return;
-            }
-            if (!world?.level?.enemies || !world?.character) return;
-            world.level.enemies.forEach(function(enemy) {
-                this.processEnemy(enemy, world);
-            }, this);
-        }
-
-        isEnabled() {
-            return window.EPL?.Sound?.isEnabled?.() === true;
-        }
-
-        stopAllActive(world) {
-            if (!world?.level?.enemies) return;
-            world.level.enemies.forEach(function(enemy) {
-                this.stopOne(enemy);
-            }, this);
-        }
-
-        processEnemy(enemy, world) {
-            let state = this.getOrCreateState(enemy);
-            let type = this.getType(enemy);
-            if (!type) return;
-            let dead = this.isDead(enemy);
-            this.handleDeath(enemy, state, dead);
-            if (dead) return;
-            this.handleAlert(enemy, state);
-            this.handleLoop(enemy, state, world, type);
-        }
-
-        getOrCreateState(enemy) {
-            if (!this.stateMap.has(enemy)) {
-                this.stateMap.set(enemy, {
-                    inRange: false,
-                    started: false,
-                    timeoutId: null,
-                    deathPlayed: false,
-                    wasAlerting: false
-                });
-            }
-            return this.stateMap.get(enemy);
-        }
-
-        getType(enemy) {
-            if (enemy instanceof smallchicken) return 'smallchicken';
-            if (enemy instanceof Chicken) return 'chicken';
-            if (enemy instanceof Endboss) return 'endboss';
-            return null;
-        }
-
-        isDead(enemy) {
-            if (typeof enemy.isDead === 'function' && enemy.isDead()) return true;
-            if (enemy.isDeadState === true) return true;
-            return enemy.energy <= 0;
-        }
-
-        handleDeath(enemy, state, dead) {
-            if (!dead || state.deathPlayed) return;
-            this.stopOne(enemy);
-            this.playOnce(SOUND_PATHS.death);
-            state.deathPlayed = true;
-        }
-
-        handleAlert(enemy, state) {
-            if (!(enemy instanceof Endboss)) return;
-            let alerting = enemy.isAlerting === true;
-            let dead = this.isDead(enemy);
-            if (!state.wasAlerting && alerting && !dead) {
-                this.playOnce(SOUND_PATHS.endbossAlert);
-            }
-            state.wasAlerting = alerting;
-        }
-
-        handleLoop(enemy, state, world, type) {
-            let inRange = this.isInRange(enemy, world);
-            if (inRange && !state.inRange) {
-                state.inRange = true;
-                state.started = false;
-                this.scheduleInitial(enemy, state, type);
-            } else if (!inRange && state.inRange) {
-                state.inRange = false;
-                this.stopOne(enemy);
-            }
-        }
-
-        isInRange(enemy, world) {
-            let charX = world.character.x;
-            let enemyX = enemy.x;
-            return Math.abs(charX - enemyX) <= RANGE;
-        }
-
-        scheduleInitial(enemy, state, type) {
-            let delay = this.randomBetween(INITIAL_DELAY_MIN, INITIAL_DELAY_MAX);
-            let self = this;
-            state.timeoutId = setTimeout(function() {
-                self.playAndReschedule(enemy, state, type);
-            }, delay);
-        }
-
-        // hier ist die zeit wann das kleine chicken einen sound macht
-        scheduleNext(enemy, state, type) {
-            let delay = this.randomBetween(REPEAT_DELAY_MIN, REPEAT_DELAY_MAX);
-            let self = this;
-            state.timeoutId = setTimeout(function() {
-                self.playAndReschedule(enemy, state, type);
-            }, delay);
-        }
-
-        playAndReschedule(enemy, state, type) {
-            if (!state.inRange || this.isDead(enemy) || !this.isEnabled()) return;
-            let src = this.getLoopSrc(type);
-            this.playOnce(src);
-            state.started = true;
-            this.scheduleNext(enemy, state, type);
-        }
-
-        getLoopSrc(type) {
-            if (type === 'smallchicken') return SOUND_PATHS.smallchickenLoop;
-            if (type === 'chicken') return SOUND_PATHS.chickenLoop;
-            return SOUND_PATHS.chickenLoop;
-        }
-
-        stopOne(enemy) {
-            let state = this.stateMap.get(enemy);
-            if (!state) return;
-            if (state.timeoutId) {
-                clearTimeout(state.timeoutId);
-                state.timeoutId = null;
-            }
-            state.inRange = false;
-            state.started = false;
-        }
-
-        playOnce(src) {
-            if (!this.isEnabled()) return;
-            let audio = new Audio(src);
-            audio.volume = 0.5;
-            audio.play().catch(function() {});
-        }
-
-        randomBetween(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
+      };
+      this.RANGE = 1200;
+      this.INIT_MIN = 400;
+      this.INIT_MAX = 1000;
+      this.REP_MIN = 2600;
+      this.REP_MAX = 3200;
     }
 
+    update(world) {
+      if (!world?.level?.enemies || !world?.character) return;
+      if (!this.isEnabled()) return this.stopAll(world.level.enemies);
+      for (let i = 0; i < world.level.enemies.length; i++) {
+        this.processEnemy(world.character, world.level.enemies[i]);
+      }
+    }
+
+    isEnabled() {
+      return window.EPL?.Sound?.isEnabled?.() === true;
+    }
+
+    processEnemy(character, enemy) {
+      const type = this.getType(enemy);
+      if (!type) return;
+      const st = this.getState(enemy);
+      if (this.isDead(enemy)) return this.handleDeath(enemy, st);
+      this.handleAlert(enemy, st);
+      this.handleLoop(character, enemy, st, type);
+    }
+
+    getType(enemy) {
+      if (enemy?.constructor?.name === 'smallchicken') return 'small';
+      if (enemy?.constructor?.name === 'Chicken') return 'chicken';
+      if (enemy?.constructor?.name === 'Endboss') return 'endboss';
+      return null;
+    }
+
+    getState(enemy) {
+      if (!this.stateMap.has(enemy)) {
+        this.stateMap.set(enemy, {
+          inRange: false,
+          timeoutId: null,
+          deathPlayed: false,
+          blocked: false,
+          wasAlerting: false,
+          currentAudio: null
+        });
+      }
+      return this.stateMap.get(enemy);
+    }
+
+    isDead(enemy) {
+      if (typeof enemy?.isDead === 'function' && enemy.isDead()) return true;
+      if (enemy?.isDeadState === true) return true;
+      if (typeof enemy?.energy === 'number') return enemy.energy <= 0;
+      return false;
+    }
+
+    handleDeath(enemy, st) {
+      if (st.blocked) return;
+      st.blocked = true;
+      this.stopEnemy(st);
+      if (!st.deathPlayed) this.playDeathOnce(st);
+    }
+
+    playDeathOnce(st) {
+      if (!this.isEnabled()) return;
+      st.deathPlayed = true;
+      const a = new Audio(this.SOUND_PATHS.death);
+      a.volume = 0.6;
+      a.play().catch(() => {});
+    }
+
+    handleAlert(enemy, st) {
+      if (enemy?.constructor?.name !== 'Endboss') return;
+      const alerting = enemy?.isAlerting === true;
+      if (!st.wasAlerting && alerting && !st.blocked) this.playAlert(st);
+      st.wasAlerting = alerting;
+    }
+
+    playAlert(st) {
+      if (!this.isEnabled()) return;
+      if (st.blocked) return;
+      this.stopCurrentAudio(st);
+      const a = new Audio(this.SOUND_PATHS.endbossAlert);
+      a.volume = 0.7;
+      st.currentAudio = a;
+      a.play().catch(() => {});
+    }
+
+    handleLoop(character, enemy, st, type) {
+      if (st.blocked) return;
+      const inRange = this.isInRange(character, enemy);
+      if (inRange && !st.inRange) return this.enterRange(enemy, st, type);
+      if (!inRange && st.inRange) return this.exitRange(st);
+    }
+
+    isInRange(character, enemy) {
+      const cx = character?.x ?? 0;
+      const ex = enemy?.x ?? 0;
+      return Math.abs(cx - ex) <= this.RANGE;
+    }
+
+    enterRange(enemy, st, type) {
+      st.inRange = true;
+      this.clearTimer(st);
+      this.stopCurrentAudio(st);
+      this.schedule(enemy, st, type, this.rand(this.INIT_MIN, this.INIT_MAX));
+    }
+
+    exitRange(st) {
+      st.inRange = false;
+      this.stopEnemy(st);
+    }
+
+    schedule(enemy, st, type, delay) {
+      st.timeoutId = setTimeout(() => {
+        if (!this.isEnabled()) return;
+        if (!st.inRange) return;
+        if (st.blocked) return;
+        if (this.isDead(enemy)) return this.handleDeath(enemy, st);
+        this.playLoopOnce(st, this.loopSrc(type));
+        this.schedule(enemy, st, type, this.rand(this.REP_MIN, this.REP_MAX));
+      }, delay);
+    }
+
+    // hier ist die zeit wann das kleine chicken einen sound macht
+    loopSrc(type) {
+      if (type === 'small') return this.SOUND_PATHS.smallLoop;
+      return this.SOUND_PATHS.chickenLoop;
+    }
+
+    playLoopOnce(st, src) {
+      this.stopCurrentAudio(st);
+      const a = new Audio(src);
+      a.volume = 0.5;
+      st.currentAudio = a;
+      a.play().catch(() => {});
+    }
+
+    stopAll(enemies) {
+      for (let i = 0; i < enemies.length; i++) this.stopOne(enemies[i]);
+    }
+
+    stopOne(enemy) {
+      const st = this.stateMap.get(enemy);
+      if (!st) return;
+      this.stopEnemy(st);
+    }
+
+    stopEnemy(st) {
+      this.clearTimer(st);
+      this.stopCurrentAudio(st);
+      st.inRange = false;
+    }
+
+    clearTimer(st) {
+      if (!st.timeoutId) return;
+      clearTimeout(st.timeoutId);
+      st.timeoutId = null;
+    }
+
+    stopCurrentAudio(st) {
+      const a = st.currentAudio;
+      if (!a) return;
+      a.pause();
+      a.currentTime = 0;
+      st.currentAudio = null;
+    }
+
+    rand(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }
+
+  function SoundToggleController(deps) {
+    this.deps = deps;
+    this.button = null;
+    this.icon = null;
+  }
+
+  SoundToggleController.prototype.init = function () {
+    this.button = document.getElementById('mute-toggle');
+    this.icon = document.getElementById('mute-icon');
+    if (!this.button || !this.icon) return;
+    this.attachListener();
+    this.updateIcon(this.deps.soundManager.isEnabled());
+  };
+
+  SoundToggleController.prototype.attachListener = function () {
+    const self = this;
+    this.button.addEventListener('click', function () {
+      const next = self.deps.soundManager.toggle();
+      self.updateIcon(next);
+    });
+  };
+
+  SoundToggleController.prototype.updateIcon = function (enabled) {
+    if (this.icon) {
+      this.icon.src = enabled ? './img/mobile/sound.png' : './img/mobile/mute.png';
+      this.icon.alt = enabled ? 'Sound an' : 'Sound aus';
+    }
+    if (this.button) {
+      this.button.setAttribute('aria-pressed', enabled ? 'false' : 'true');
+    }
+  };
+
+  if (!window.EPL.Controllers.SoundToggle) {
+    window.EPL.Controllers.SoundToggle = SoundToggleController;
+  }
+
+  if (!window.EPL.EnemySfx) {
     window.EPL.EnemySfx = new EnemySfxManager();
+  }
 })();
